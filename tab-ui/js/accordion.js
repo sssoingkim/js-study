4fb48b9f29e4d169cfcc1fc5b8c111ed664f9d73
songyi <songee-kim@nts-corp.com>
2022-03-29 02:08:14 +0900

    Fix Tab&Accordion - Arrow Function, this, addEventListner

--- tab-ui/js/accordion.js
+++ tab-ui/js/accordion.js
@@ -1,22 +1,59 @@
 const accordionItem = document.querySelectorAll(".book_item");
 
-for(let i = 0; i < accordionItem.length; i++) {
-  /* 각 아코디언버튼에 클릭 이벤트 추가 */
-  accordionItem[i].firstElementChild.addEventListener("click", function() {
-    const accordionIcon = this.lastElementChild;
-    const accordionContent = this.parentNode.lastElementChild;
-    let checkExpanded = this.ariaExpanded;
+// for(let i = 0; i < accordionItem.length; i++) {
+//   각 아코디언버튼에 클릭 이벤트 추가
+//   accordionItem[i].firstElementChild.addEventListener("click", function() {
+//     const accordionIcon = this.lastElementChild;
+//     const accordionContent = this.parentNode.lastElementChild;
+//     let checkExpanded = this.ariaExpanded;
 
-    /* 선택한 아코디언버튼의 ARIA 속성 및 아이콘 변경 / 콘텐츠 노출 토글 */
-    if(checkExpanded === "false") {
-      checkExpanded = "true;"
-      accordionIcon.innerText = "▲";
-      accordionContent.classList.add(OPEN_CLASS);
-    } else {
-      checkExpanded = "false";
-      accordionIcon.innerText = "▼";
-      accordionContent.classList.remove(OPEN_CLASS);
-    }
-    this.ariaExpanded = checkExpanded;
-  });
+//     선택한 아코디언버튼의 ARIA 속성 및 아이콘 변경 / 콘텐츠 노출 토글
+//     if(checkExpanded === "false") {
+//       checkExpanded = "true;"
+//       accordionIcon.innerText = "▲";
+//       accordionContent.classList.add(OPEN_CLASS);
+//     } else {
+//       checkExpanded = "false";
+//       accordionIcon.innerText = "▼";
+//       accordionContent.classList.remove(OPEN_CLASS);
+//     }
+//     this.ariaExpanded = checkExpanded;
+//   });
+// }
+
+const hideContent = (check, icon, content) => {
+  check = "false";
+  icon.innerText = "▼";
+  content.classList.remove(OPEN_CLASS);
 }
+
+const openContent = (check, icon, content) => {
+  check = "true;"
+  icon.innerText = "▲";
+  content.classList.add(OPEN_CLASS);
+}
+
+const resetContent = () => {
+  accordionItem.forEach((item) => {
+    const accordionButton = item.firstElementChild;
+    hideContent(accordionButton.ariaExpanded, accordionButton.lastElementChild, accordionButton.parentNode.lastElementChild);
+  })
+}
+
+const onClickAccordion = (e) => {
+  let checkExpanded = e.currentTarget.ariaExpanded;
+  const accordionIcon = e.currentTarget.lastElementChild;
+  const accordionContent = e.currentTarget.parentNode.lastElementChild;
+
+  if(checkExpanded === "false") {
+    resetContent();
+    openContent(checkExpanded, accordionIcon, accordionContent);
+  } else {
+    hideContent(checkExpanded, accordionIcon, accordionContent);
+  }
+  this.ariaExpanded = checkExpanded;
+}
+
+accordionItem.forEach((item) => {
+  item.firstElementChild.addEventListener("click", onClickAccordion)
+})
--- tab-ui/js/tab.js
+++ tab-ui/js/tab.js
@@ -4,23 +4,43 @@ const tabPanel = document.querySelectorAll(".panel_item");
 const ACTIVE_CLASS = "active";
 const OPEN_CLASS = "open";
 
-for(let i = 0; i < tabItem.length; i++) {
-  /* 각 탭버튼에 클릭 이벤트 추가 */
-  tabItem[i].addEventListener("click", function() {
-    /* 클래스 초기화(?) */
-    for(let j = 0; j < tabItem.length; j++) {
-      tabItem[j].classList.remove(ACTIVE_CLASS);
-      tabItem[j].ariaSelected = false;
-      tabPanel[j].classList.remove(OPEN_CLASS);
-    }
+// for(let i = 0; i < tabItem.length; i++) {
+//   tabItem[i].addEventListener("click", function() {
+//     for(let j = 0; j < tabItem.length; j++) {
+//       tabItem[j].classList.remove(ACTIVE_CLASS);
+//       tabItem[j].ariaSelected = false;
+//       tabPanel[j].classList.remove(OPEN_CLASS);
+//     }
     
-    /* 선택한 탭버튼에 클래스 추가 및 ARIA 속성 변경 */
-    this.classList.add(ACTIVE_CLASS);
-    this.ariaSelected = true;
+//     this.classList.add(ACTIVE_CLASS);
+//     this.ariaSelected = true;
 
-    /* 선택한 탭버튼과 연결된 탭패널에 클래스 추가 */
-    const tabId = this.parentNode.id;
-    // parentNode VS parentElement
-    document.getElementById(tabId + "_panel").classList.add(OPEN_CLASS);
-  });
+//     const tabId = this.parentNode.id;
+//     document.getElementById(tabId + "_panel").classList.add(OPEN_CLASS);
+//   });
+// }
+
+const resetTab = () => {
+  tabItem.forEach((item) => {
+    item.classList.remove(ACTIVE_CLASS);
+    item.ariaSelected = false;
+  })
+  tabPanel.forEach((item) => {
+    item.classList.remove(OPEN_CLASS);
+  })
 }
+
+const onClickTab = (e) => {
+  resetTab();
+
+  e.currentTarget.classList.add(ACTIVE_CLASS);
+  e.currentTarget.ariaSelected = true;
+
+  const tabId = e.currentTarget.parentNode.id;
+  document.getElementById(`${tabId}_panel`).classList.add(OPEN_CLASS);
+}
+
+tabItem.forEach((item) => {
+  item.addEventListener("click", onClickTab);
+  //onClickTab(매개변수)가 들어갈 경우 함수를 실행시키게 됨.
+})
