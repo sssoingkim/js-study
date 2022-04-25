2bd79a3d1bce0e7d5116765824ac2655ff66d79b
songyi <songee-kim@nts-corp.com>
2022-04-26 01:21:32 +0900

    Develop clock by using CLASS

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -2,16 +2,46 @@ const hour = document.querySelector('#hours');
 const min = document.querySelector('#minutes');
 const sec = document.querySelector('#seconds');
 
-function updateTime() {
-  const time = new Date();
+class Clock {
+  constructor() {}
+  getTime() {
+    const date = new Date();
 
-  hour.innerText = time.getHours().toString().padStart(2,'0');
-  min.innerText = time.getMinutes().toString().padStart(2,'0');
-  sec.innerText = time.getSeconds().toString().padStart(2,'0');
+    const _hour = date.getHours().toString().padStart(2,'0');
+    const _min = date.getMinutes().toString().padStart(2,'0');
+    const _sec = date.getSeconds().toString().padStart(2,'0');
 
-  setTimeout(updateTime, 1000);
+    return { _hour, _min, _sec}
+  }
 }
 
-window.onload = function() {
-  updateTime();
+window.onload = () => {
+  const clock = new Clock();
+
+  setInterval(() => {
+    const time = clock.getTime();
+
+    hour.innerText = time._hour;
+    min.innerText = time._min;
+    sec.innerText = time._sec;
+  }, 500);
 }
+
+
+// function getCurrentTime() {
+//   const time = new Date();
+
+//   hour.innerText = time.getHours().toString().padStart(2,'0');
+//   min.innerText = time.getMinutes().toString().padStart(2,'0');
+//   sec.innerText = time.getSeconds().toString().padStart(2,'0');
+
+// }
+
+// function updateTime() {
+//   getCurrentTime();
+//   setTimeout(updateTime, 1000);
+// }
+
+// window.onload = function() {
+//   updateTime();
+// }
