5c5ef84cf126a98bbcdfffe0703d6bd7b80be824
songyi <songee-kim@nts-corp.com>
2022-06-28 23:58:32 +0900

    Apply the correct code

--- 07.star-rating/js/rating.js
+++ 07.star-rating/js/rating.js
@@ -5,74 +5,56 @@ class Star {
   constructor(el, count, callback) {
     this._container = document.querySelector(el);
     this._count = count;
-    this._getStar = callback;
-    this._init = this.createStar();
+    this._getStar = callback; //멤버변수 이름도 _callback으로 써주는게 더 안헷갈리고 좋을까?
+    this._active = -1;
+    this.init();
+    this.bindEvents(); //왜 여기서 bindEvents?
   }
-  destroy() {
-    this.addEvent.buttons.forEach((element, index) => {
-      //이벤트 이렇게 소거하는게 맞나..?ㅠㅠ
-      element.removeEventListener('mouseover', this.changeWhenOver(this.addEvent.buttons, this.addEvent.tempIndex, index));
-      element.removeEventListener('click', this._getStar(index+1));
-    });
-  }
-  createStar() {
-    const button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    button.classList.add('button_rating');
+  init() {
+    const fragment = document.createDocumentFragment();
 
-    const icon = document.createElement('i');
-    icon.classList.add('fa');
-    icon.classList.add(EMPTY_STAR_CLASS);
-    button.appendChild(icon);
-    
+    for(let i = 1; i <= this._count; i++) {
+      const button = document.createElement('button');
+      button.setAttribute('type', 'button');
+      button.classList.add('button_rating');
+      button.dataset.ratingVal = i;
+
+      const icon = document.createElement('i');
+      icon.classList.add('fa');
+      icon.classList.add(EMPTY_STAR_CLASS);
+      icon.dataset.ratingVal = i;
+      button.appendChild(icon);
+      fragment.appendChild(button);
+    }
+
+    this._container.appendChild(fragment);
+  }
+  fillColor(val) {
     for(let i = 0; i < this._count; i++) {
-      this._container.appendChild(button.cloneNode(true));
-    }
-    this.addEvent();
-  }
-  addEvent() {
-    const buttons = this._container.querySelectorAll('.button_rating');
-    let tempIndex = 0;
-    let value = 0;
-
-    buttons.forEach((element, index) => {
-      element.onmouseover = () => {
-        tempIndex = this.changeWhenOver(buttons, tempIndex, index);
+      if(i < val) {
+        this._container.children[i].children[0].classList.add(FULL_STAR_CLASS);
+      } else {
+        this._container.children[i].children[0].classList.remove(FULL_STAR_CLASS);
       }
-      element.onclick = () => {
-        value = index;
-        this._getStar(index+1);
-      }
-    });
-
-    this._container.onmouseleave = () => {
-      this.changeWhenLeave(buttons, tempIndex, value);
     }
   }
-  changeWhenOver(array, temp, idx) {
-    if(temp <= idx) {
-      for(let i = 0; i < idx+1; i++) {
-        array[i].firstChild.classList.replace(EMPTY_STAR_CLASS, FULL_STAR_CLASS);
-      }
-    } else {
-      for(let i = idx+1; i < this._count; i++) {
-        array[i].firstChild.classList.replace(FULL_STAR_CLASS, EMPTY_STAR_CLASS);
-      }
-    }
-    temp = idx;
-
-    return temp;
+  onMouseOver(e) {
+    const ratingVal = e.target.dataset.ratingVal; //button을 가리키게 하는 좋은 방법은?
+    if(!ratingVal) return;
+    this.fillColor(ratingVal);
   }
-  changeWhenLeave(array, temp, val) {
-    if(temp <= val) {
-      for(let i = temp+1; i <= val; i++) {
-        array[i].firstChild.classList.replace(EMPTY_STAR_CLASS, FULL_STAR_CLASS);
-      }
-    } else {
-      for(let i = temp; i > val; i--) {
-        array[i].firstChild.classList.replace(FULL_STAR_CLASS, EMPTY_STAR_CLASS);
-      }
-    }
+  onMouseLeave() {//e 매개변수는 왜 넣어주지? 쓰임이 없는디.. 지워도 괜찮은가?
+    this.fillColor(this._active);
+  }
+  onMouseClick(e) {
+    this._active = e.target.dataset.ratingVal;
+    this.fillColor(this._active);
+    this._getStar(this._active);
+  }
+  bindEvents() {
+    this._container.addEventListener('mouseover', this.onMouseOver.bind(this));
+    this._container.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+    this._container.addEventListener('click', this.onMouseClick.bind(this));
   }
 }
 
